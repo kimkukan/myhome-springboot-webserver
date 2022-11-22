a9bef7220c2382086cadae03feb4445a134ddb86
kimkukan <kimkukan@naver.com>
2022-11-23 04:46:27 +0900

    PostRepository 작성

---
+++ src/main/java/com/kimkukan/springboot/domain/post/Post.java
@@ -0,0 +1,36 @@
+package com.kimkukan.springboot.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@NoArgsConstructor
+@Entity //Entity 클래스는 DB테이블과 연계되는 클래스, setter를 사용하지 않는다
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk자동생성규정
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Post(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/kimkukan/springboot/domain/post/PostRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.kimkukan.springboot.domain.post;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest //h2 가상DB를 자동실행도 해줌
+public class PostRepositoryTest {
+    
+    @Autowired
+    PostRepository postRepository;
+    
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+    //단위테스트가 끝나고 메모리를 비워주는 역할
+    
+    @Test
+    public void table_call_test() {
+        String title = "테스트게시글";
+        String content = "테스트 게시글 본문입니다. 어쩌구 저쩌구";
+        
+        postRepository.save(Post.builder()
+                .title(title)
+                .content(content)
+                .author("kimkukan@gmail.com")
+                .build());
+        
+        List<Post> postList = postRepository.findAll();
+
+        Post post = postList.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getContent()).isEqualTo(content);
+    }
+}
