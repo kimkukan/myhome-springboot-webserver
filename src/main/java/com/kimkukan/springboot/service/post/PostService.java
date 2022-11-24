4ab88a0a0919608284d53e5cee756b41f88e8212
kimkukan <kimkukan@naver.com>
2022-11-25 03:45:55 +0900

    PostApiController 생성

---
+++ src/main/java/com/kimkukan/springboot/service/post/PostService.java
@@ -0,0 +1,18 @@
+package com.kimkukan.springboot.service.post;
+
+import com.kimkukan.springboot.domain.post.PostRepository;
+import com.kimkukan.springboot.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long save(PostSaveRequestDto requestDto){
+        return postRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/kimkukan/springboot/web/PostApiController.java
@@ -0,0 +1,19 @@
+package com.kimkukan.springboot.web;
+
+import com.kimkukan.springboot.service.post.PostService;
+import com.kimkukan.springboot.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostService postService;
+
+    @PostMapping
+    public Long save(@RequestBody PostSaveRequestDto requestDto){
+        return postService.save(requestDto);
+    }
+}
