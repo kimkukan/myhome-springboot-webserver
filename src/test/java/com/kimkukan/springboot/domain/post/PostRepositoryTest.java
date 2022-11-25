3af6d45910be87fa58e313f2dde54254fd0fe731
kimkukan <kimkukan@naver.com>
2022-11-25 13:32:29 +0900

    DateTime 기능 테스트 구성완료

--- src/test/java/com/kimkukan/springboot/domain/post/PostRepositoryTest.java
+++ src/test/java/com/kimkukan/springboot/domain/post/PostRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -39,5 +40,26 @@ public class PostRepositoryTest {
         Post post = postList.get(0);
         assertThat(post.getTitle()).isEqualTo(title);
         assertThat(post.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_register(){
+        LocalDateTime now = LocalDateTime.of(2022,11,25,9,0,0);
+        postRepository.save(Post.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build());
+
+        //when
+        List<Post> postList = postRepository.findAll();
+
+        //then
+        Post post = postList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>>> createDate="+post.getCreatedDate()+", modifiedDate="+post.getModifiedDate());
+
+        assertThat(post.getCreatedDate().isAfter(now));
+        assertThat(post.getModifiedDate().isAfter(now));
     }
 }
